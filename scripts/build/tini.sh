7fa6e9f7732b97535046f42c5e261db8e90f9bfb
Erhhung Yuan <erhhung@gmail.com>
2025-03-10 17:20:02 -0700

    Fix: CFLAGS refactoring error

--- scripts/build/tini.sh
+++ scripts/build/tini.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # shellcheck disable=SC2148 # Tips depend on target shell
+# shellcheck disable=SC2178 # Variable was used as an array
 
 echo "::group::Build tini"
 trap 'echo "::endgroup::"' EXIT
@@ -11,10 +12,11 @@ dnf install -y cmake glibc-static
 git clone -q https://github.com/krallin/tini
 cd tini
 
-export CFLAGS="
+CFLAGS=(
   -DPR_SET_CHILD_SUBREAPER=36
   -DPR_GET_CHILD_SUBREAPER=37
-  "
+)
+export CFLAGS="${CFLAGS[*]}"
 cmake .
 make -sj"$(nproc)"
 # installs into /usr/local/bin
